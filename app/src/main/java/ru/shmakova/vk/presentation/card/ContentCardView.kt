0fc35a7588637fb5a94ff66b8b93308ddb1f8217
Anastasia Shmakova <shmakova-a@yandex-team.ru>
2018-11-10 01:04:47 +0300

    Add bottom buttons

---
+++ app/src/main/java/ru/shmakova/vk/presentation/card/ContentCardView.kt
@@ -0,0 +1,8 @@
+package ru.shmakova.vk.presentation.card
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+
+class ContentCardView(context: Context?) : ConstraintLayout(context) {
+
+}
