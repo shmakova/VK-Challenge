19d90257b7ec54636f09d9441fbadbc9045ddf29
Anastasia Shmakova <shmakova-a@yandex-team.ru>
2018-11-10 11:10:28 +0300

    Content card view

--- app/src/main/java/ru/shmakova/vk/presentation/card/ContentCardView.kt
+++ app/src/main/java/ru/shmakova/vk/presentation/card/ContentCardView.kt
@@ -2,7 +2,56 @@ package ru.shmakova.vk.presentation.card
 
 import android.content.Context
 import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import ru.shmakova.vk.R
 
-class ContentCardView(context: Context?) : ConstraintLayout(context) {
+class ContentCardView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
+    private val avatarView: ImageView
+    private val nameView: TextView
+    private val dateView: TextView
+    private val photoView: ImageView
+    private val textView: TextView
+
+    init {
+        inflate(context, R.layout.content_card_view, this)
+        avatarView = findViewById(R.id.avatar)
+        nameView = findViewById(R.id.name)
+        dateView = findViewById(R.id.date)
+        photoView = findViewById(R.id.attachment)
+        textView = findViewById(R.id.text)
+    }
+
+    fun setAvatar(url: String) {
+        Glide.with(this)
+            .load(url)
+            .apply(RequestOptions.circleCropTransform())
+            .into(avatarView)
+    }
+
+    fun setName(name: String) {
+        nameView.text = name
+    }
+
+    fun setDate(date: String) {
+        dateView.text = date
+    }
+
+    fun setPhoto(url: String) {
+        Glide.with(this)
+            .load(url)
+            .into(photoView)
+    }
+
+    fun setText(text: String) {
+        textView.text = text
+    }
 }
--- app/src/main/java/ru/shmakova/vk/presentation/main/MainActivity.kt
+++ app/src/main/java/ru/shmakova/vk/presentation/main/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import dagger.android.AndroidInjection
 import ru.shmakova.vk.R
+import ru.shmakova.vk.presentation.card.ContentCardView
 import timber.log.Timber
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), MainView {
+
+    private lateinit var contentCardView: ContentCardView
 
     @Inject
     lateinit var presenter: MainPresenter
@@ -16,11 +19,20 @@ class MainActivity : AppCompatActivity(), MainView {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        contentCardView = findViewById(R.id.card)
         presenter.bindView(this)
     }
 
     override fun render(state: MainViewState) {
         Timber.e("Render state: %s", state)
+        if (state.newsFeed != null) {
+            val news = state.newsFeed.response.items.first()
+            val profile = state.newsFeed.response.profiles.first()
+            contentCardView.setAvatar(profile.photo)
+            contentCardView.setName("${profile.firstName} ${profile.lastName}")
+            contentCardView.setDate("${news.date}")
+            contentCardView.setText(news.text)
+        }
     }
 
     override fun onDestroy() {
